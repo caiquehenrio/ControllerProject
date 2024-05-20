6b78c46e3faa5afb296532ac42c717a76723a749
Caio Henrique Pereira <132082169+caiquehenrio@users.noreply.github.com>
2024-05-20 16:17:14 -0300

    Create AdminUserLoggedFilter.cs

---
+++ Filters/AdminUserLoggedFilter.cs
@@ -0,0 +1,56 @@
+using ControllerProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerProject.Filters
+{
+
+    public class AdminUserLoggedFilter : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+
+            string userSession = context.HttpContext.Session.GetString("UserSession");
+
+            if (string.IsNullOrEmpty(userSession))
+            {
+
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+
+            }
+            else 
+            {
+
+                UserModel user = JsonConvert.DeserializeObject<UserModel>(userSession);
+
+                if(user == null) 
+                {
+
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+                    
+                }
+
+                if(user.Profile != Enums.ProfileEnum.Admin) 
+                {
+
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Restricted" }, { "action", "Index" } });
+
+                }
+
+            }
+
+            base.OnActionExecuting(context);
+        
+        }
+
+    }
+
+}
