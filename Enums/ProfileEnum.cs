6b7d93c4aa9c562fa7bea9609115ccb753d5de15
Caio Henrique Pereira <132082169+caiquehenrio@users.noreply.github.com>
2024-05-20 16:16:39 -0300

    Create ProfileEnum.cs

---
+++ Enums/ProfileEnum.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerProject.Enums
+{
+
+    public enum ProfileEnum
+    {
+
+        Admin = 1,
+
+        Basic = 2
+
+    }
+
+}
